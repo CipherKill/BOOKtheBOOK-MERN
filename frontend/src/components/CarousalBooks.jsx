76924a88d0bf87ac5472150816f7b07bf08211c4
Alan Raju (CipherKill) <anonymousisactive@gmail.com>
2022-05-24 23:00:20 +0530

    added some route navigation for UI

--- frontend/src/components/CarousalBooks.jsx
+++ frontend/src/components/CarousalBooks.jsx
@@ -3,10 +3,16 @@ import carousal0 from '../assets/carousal0.jpg'
 import carousal1 from '../assets/carousal1.jpg'
 import carousal2 from '../assets/carousal2.jpg'
 import carousal3 from '../assets/carousal3.jpg'
+import {useNavigate} from 'react-router-dom'
 
 function CarousalBooks() {
+    const navigate=useNavigate()
+    const handleClick=()=>{
+        navigate('/books')
+    }
+
   return (
-    <Carousel className=''>
+    <Carousel className='finger' onClick={handleClick}>
         <Carousel.Item>
         <img
             className="d-block w-100"
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,26 @@
 import CarousalBooks from '../components/CarousalBooks'
 import Books from './Books'
 import {Col,Row} from 'react-bootstrap'
+import {useNavigate} from 'react-router-dom'
 
 import ad1 from '../assets/leftpic.jpg'
 import ad2 from '../assets/rightpic.jpg'
 
 function Home() {
+
+  const navigate=useNavigate();
+  const handleClick=()=>navigate('/books');
+
   return (
     <>
         <h1 className='text-center h1 my-4 fw-bold'>Welcome to  BOOKtheBOOK</h1>
         <CarousalBooks/>
-        <Row className='mt-5 pop'>
+        <Row className='mt-5 pop' onClick={handleClick}>
           <Col className='d-flex justify-content-center'>
-            <img src={ad1} alt='leftside AD'/>
+            <img src={ad1} alt='leftside AD' width="470rem" className='finger'/>
           </Col>
           <Col className='d-flex justify-content-center'>
-          <img src={ad2} alt='rightside AD'/>
+          <img src={ad2} alt='rightside AD' width="470rem" className='finger'/>
           </Col>
         </Row>
         <Books/>
