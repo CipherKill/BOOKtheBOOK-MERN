8f194c838b64d4bdaffca7e900695d767a180276
Alan Raju (CipherKill) <anonymousisactive@gmail.com>
2022-05-25 15:23:52 +0530

    Added route for category search (backend)

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -7,6 +7,13 @@ const getAllBooks=asyncHandler(async (req,res)=>{
     res.json(books)
 });
 
+const getBooksOnCategory=asyncHandler(async (req,res)=>{
+    const category=req.params.category
+    const bookq=await Books.find({genre:category})
+    res.json(bookq)
+})
+
 module.exports={
-    getAllBooks
+    getAllBooks,
+    getBooksOnCategory
 }
--- backend/routes/booksRoutes.js
+++ backend/routes/booksRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const router=express.Router()
 
-const {getAllBooks}=require('../controllers/booksController');
+const {getAllBooks,getBooksOnCategory}=require('../controllers/booksController');
 
 router.get('/all',getAllBooks);
+router.get('/:category',getBooksOnCategory)
 
 module.exports=router;
